14309692353984753dcb373f2f6c6d79f4feb027
lyc32 <liyc930826@gmail.com>
2023-06-07 10:28:12 -0500

    exercise 5 to 8

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function()
+const sumAll = function(a,b)
 {
+    if((typeof a === 'number') && ( typeof b === 'number') && (a > 0) && (b > 0))
+    {
 
+        return b>a ? ((a + b)*(b - a + 1)/2) : ((a + b)*(a - b + 1)/2) ;
+    }
+    else
+    {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,65 @@
-const add = function() {
-	
+const add = function(number1, number2)
+{
+  return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2)
+{
+  return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array)
+{
+  let sum = 0;
+  for(let i = 0; i < array.length; i++)
+  {
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array)
+{
+  if(array.length == 0)
+  {
+    return 0;
+  }
+  else
+  {
+    let sum = 1;
+    for(let i = 0; i < array.length; i++)
+    {
+      sum = sum * array[i];
+    }
+    return sum;
+  }
 };
 
-const power = function() {
-	
+const power = function(number, power)
+{
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num)
+{
+  if (num < 0)
+  {
+    return -1;
+  }
+  else if (num == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    let result = 1;
+    for(let i = 1; i<=num; i++)
+    {
+      result = result*i;
+    }
+    return result;
+  }
+
 };
 
 // Do not edit below this line
